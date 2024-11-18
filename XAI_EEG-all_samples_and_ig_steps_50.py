87977919fc591ddbc6055f734b1f4714dc3ff212
abrhm11 <100416339+abrhm11@users.noreply.github.com>
2024-11-18 19:50:32 +0100

    added text, updated mean calculation

--- XAI_EEG-all_samples_and_ig_steps_50.py
+++ XAI_EEG-all_samples_and_ig_steps_50.py
@@ -99,7 +99,7 @@ all_indices = [indices_0, indices_1, indices_2, indices_3, indices_4]
 folder_path = "eeg_frequencies_relevance_plots"
 os.makedirs(folder_path, exist_ok=True)
 
-pdf_file = PdfPages(os.path.join(folder_path, "eeg_frequencies_relevance_plots.pdf"))
+pdf_file = PdfPages(os.path.join(folder_path, "eeg_frequencies_relevance_plots_edit.pdf"))
 
 model.train()
 ig = IntegratedGradients(model) # get an instance using our model
@@ -109,6 +109,8 @@ internal_batch_size = 10
 # Liste, um die durchschnittlichen (positiv gewichteten) Frequenzen für jedes indices-Array zu speichern
 overall_average_frequencies_pos_list = []
 overall_average_frequencies_neg_list = []
+mean_attributions_list = []
+all_frequencies = None
 
 phases = ["wake", "REM", "N1", "N2", "N3"]
 
@@ -116,9 +118,9 @@ phases = ["wake", "REM", "N1", "N2", "N3"]
 for i, indices_i in enumerate(all_indices):
     print(f"Processing segments of {phases[i]} stage...")  # Ausgabe für das aktuelle indices-Array
 
-    all_frequencies = None
-    all_weighted_pos_attributions = []
-    all_weighted_neg_attributions = []
+    #all_weighted_pos_attributions = []
+    #all_weighted_neg_attributions = []
+    all_weighted_attributions = []
     all_amplitudes = []
     
     # Innere Schleife über patients
@@ -161,30 +163,40 @@ for i, indices_i in enumerate(all_indices):
             frequencies = np.fft.fftfreq(len(x_segment_dct), d=1/100)
             
             # Positive und negative Attributionswerte normalisieren
-            attributions_magnitude_pos = np.where(attributions_magnitude > 0, attributions_magnitude, 0)
-            attributions_magnitude_neg = np.where(attributions_magnitude < 0, attributions_magnitude, 0)
+            #attributions_magnitude_pos = np.where(attributions_magnitude > 0, attributions_magnitude, 0)
+            #attributions_magnitude_neg = np.where(attributions_magnitude < 0, attributions_magnitude, 0)
             
-            sum_attributions_pos = np.sum(attributions_magnitude_pos)
-            sum_attributions_neg = np.sum(attributions_magnitude_neg)
+            #sum_attributions_pos = np.sum(attributions_magnitude_pos)
+            #sum_attributions_neg = np.sum(attributions_magnitude_neg)
+
+            sum_attributions = np.sum(np.abs(attributions_magnitude))
             
             # Relativen Attributionswert pro Frequenz berechnen und speichern
             if all_frequencies is None:
                 all_frequencies = frequencies
             
-            relative_pos_attributions = attributions_magnitude_pos / sum_attributions_pos
-            relative_neg_attributions = attributions_magnitude_neg / sum_attributions_neg
+            #relative_pos_attributions = attributions_magnitude_pos / sum_attributions_pos
+            #relative_neg_attributions = attributions_magnitude_neg / sum_attributions_neg
             
-            all_weighted_pos_attributions.append(relative_pos_attributions)
-            all_weighted_neg_attributions.append(relative_neg_attributions)
+            #all_weighted_pos_attributions.append(relative_pos_attributions)
+            #all_weighted_neg_attributions.append(relative_neg_attributions)
+
+            relative_attributions = attributions_magnitude/sum_attributions
+
+            all_weighted_attributions.append(relative_attributions)
     
             # Amplituden speichern
             amplitudes = np.abs(x_segment_dct).numpy()
             all_amplitudes.append(amplitudes)
     
     # Mittelwerte der Attributionswerte über alle Segmente hinweg für jede Frequenz berechnen (sollte eigentlich auch gewichtet sein, unterscheidet sich aber hier nur in einem Faktor)
-    mean_weighted_pos_attributions = np.mean(all_weighted_pos_attributions, axis=0)
-    mean_weighted_neg_attributions = np.mean(all_weighted_neg_attributions, axis=0)
-    mean_weighted_attributions = mean_weighted_pos_attributions + mean_weighted_neg_attributions
+    #mean_weighted_pos_attributions = np.mean(all_weighted_pos_attributions, axis=0)
+    #mean_weighted_neg_attributions = np.mean(all_weighted_neg_attributions, axis=0)
+    #mean_weighted_attributions = mean_weighted_pos_attributions + mean_weighted_neg_attributions
+    mean_attributions = np.mean(all_weighted_attributions, axis=0)
+
+    mean_weighted_pos_attributions = np.where(mean_attributions > 0, mean_attributions, 0)
+    mean_weighted_neg_attributions = np.where(mean_attributions < 0, mean_attributions, 0)
 
     # Mittelwerte der Amplituden über alle Segmente hinweg für jede Frequenz berechnen
     mean_amplitudes = np.mean(all_amplitudes, axis=0)
@@ -196,6 +208,7 @@ for i, indices_i in enumerate(all_indices):
     # Speichern der durchschnittlichen Frequenzen
     overall_average_frequencies_pos_list.append(overall_average_frequency_pos)
     overall_average_frequencies_neg_list.append(overall_average_frequency_neg)
+    mean_attributions_list.append(mean_attributions)
     
     # Plot 1: Average Amplitudes colored by weighted attributions
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
@@ -207,7 +220,7 @@ for i, indices_i in enumerate(all_indices):
     bars = ax1.bar(
         all_frequencies,
         mean_amplitudes,
-        color=cmap(mean_weighted_attributions / np.max(mean_weighted_attributions)),
+        color=cmap(((mean_attributions / np.max(np.abs(mean_attributions)))+1)/2),
         label="coloured by weighted attributions",
         width=0.2
     )
@@ -224,21 +237,22 @@ for i, indices_i in enumerate(all_indices):
     # Plot 2: Average Weighted Positive and Negative Attributions
     cmap_pos = plt.cm.Greens
     cmap_neg = plt.cm.Reds
+
     
     # Positive weighted attributions
     bars_pos = ax2.bar(
         all_frequencies,
         mean_weighted_pos_attributions,
-        color=cmap_pos(mean_weighted_pos_attributions / np.max(mean_weighted_pos_attributions)),
+        color=cmap_pos(mean_weighted_pos_attributions / np.max(np.abs(mean_attributions))),
         label="Positively weighted attributions",
         width=0.2
     )
-    
+        
     # Negative weighted attributions (negated for visual clarity)
     bars_neg = ax2.bar(
         all_frequencies,
-        -mean_weighted_neg_attributions,
-        color=cmap_neg(mean_weighted_neg_attributions / np.max(mean_weighted_neg_attributions)),
+        mean_weighted_neg_attributions,
+        color=cmap_neg(-mean_weighted_neg_attributions / np.max(np.abs(mean_attributions))),
         label="Negatively weighted attributions",
         width=0.2
     )
@@ -248,7 +262,7 @@ for i, indices_i in enumerate(all_indices):
     ax2.set_ylabel("Average Attribution")
     ax2.set_title(f"Average weighted positive/negative attributions in {phases[i]} stage")
     ax2.set_xlim(0, 40)
-    ax2.set_ylim(-np.max(mean_weighted_neg_attributions) * 1.1, np.max(mean_weighted_pos_attributions) * 1.1)
+    ax2.set_ylim(np.min(mean_weighted_neg_attributions) * 1.1, np.max(mean_weighted_pos_attributions) * 1.1)
     ax2.grid(axis='y', linestyle='--', alpha=0.7)
     ax2.legend()
 
@@ -269,6 +283,13 @@ for i, avg_freq in enumerate(overall_average_frequencies_pos_list):
 text_output += "\nAverage (negativ weighted) frequencies for all sleep stages:\n"
 for i, avg_freq in enumerate(overall_average_frequencies_neg_list):
     text_output += f"{phases[i]}: {avg_freq:.2f} Hz\n"
+
+text_output += "\nMost dominant (positive or negative attributed) frequencies compared to the other sleep stages:\n"
+sum_mean_attributions_list = np.sum(mean_attributions_list)
+for i, attr in enumerate(mean_attributions_list):
+    attr_arg = np.argmax(attr/sum_mean_attributions_list)
+    frequency = all_frequencies[np.argmax(attr/sum_mean_attributions_list)]
+    text_output += f"{phases[i]}: {frequency:.2f} Hz at (relative) attribution {attr[attr_arg]:.2f}\n"
 
 # Add the text to the figure
 plt.text(0.5, 0.5, text_output, fontsize=12, ha='center', va='center')
